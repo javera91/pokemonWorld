5c10712fd6550198438b1b96b9087a3a38975065
javera91 <146044960+javera91@users.noreply.github.com>
2024-06-25 18:07:06 -0500

    cd

---
+++ frontend/src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  templateUrl: './welcome.component.html',
+  imports: [
+    
+  ],
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
