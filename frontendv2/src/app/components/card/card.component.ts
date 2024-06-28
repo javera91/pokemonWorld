650eebb3ebb4408c34c86b22e4549c2c9f1ec290
javera91 <146044960+javera91@users.noreply.github.com>
2024-06-28 17:01:36 -0500

    c

--- frontendv2/src/app/components/card/card.component.ts
+++ frontendv2/src/app/components/card/card.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {NzCardComponent, NzCardMetaComponent} from "ng-zorro-antd/card";
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [
-    
+    NzCardComponent,
+    NzCardMetaComponent
+
   ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
